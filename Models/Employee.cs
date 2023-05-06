c220ea03ee8b2acf81411cce7537a52d4edb0038
AhmedYasser-js <ahmedkasso22@gmail.com>
2023-05-05 21:34:51 -0700

    HRSystem Project

---
+++ Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models
+{
+    public class Employee
+    {
+
+
+        //name
+        [Required(ErrorMessage = "Enter Nmae")]
+        [StringLength(20, MinimumLength = 3)]
+        public string? EmployeeName { get; set; }
+
+
+        //Department
+        [Required(ErrorMessage = "Select Department")]
+        public string? Department { get; set; }
+
+        //is active
+        public bool isActive { get; set; }
+
+        //job Title
+        [Required(ErrorMessage = "job Title is Missing")]
+        [DataType(DataType.Text)]
+        public string? JobTitle { get; set; }
+
+        //Date of Birth 
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        //EmailAddress
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? EmailAddress { get; set; }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿//using System.Collections.Generic;
+
+namespace Application.Models
+{
+    public class Repository
+    {
+        public static List<Employee> employeesList = new List<Employee>();
+
+        public static IEnumerable<Employee> GetEmployees()
+        {
+            return employeesList;
+        }
+
+        public static void AddEmployee(Employee emp)
+        {
+            employeesList.Add(emp);
+        }
+    }
+}
